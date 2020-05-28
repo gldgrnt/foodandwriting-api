74a3fda17f5c831f088190e021759ed02f1f24e8
Gilad Grant <ggrant966@gmail.com>
2020-05-28 15:42:50 +0100

    Change exports to use `validate` function

--- app/middleware/validation/comments.js
+++ app/middleware/validation/comments.js
@@ -1,17 +1,18 @@
 const { param, body } = require('express-validator')
+const { validate } = require('../../helpers').validation
 
-exports.addComment = [
+exports.addComment = validate([
     body('postId').not().isEmpty().isString().trim(),
     body('displayName').not().isEmpty().isString().escape().trim(),
     body('postSlug').not().isEmpty().isSlug().trim(),
     body('parentCommentId').toInt(),
     body('text').not().isEmpty().escape().trim()
-]
+])
 
-exports.approveComment = [
+exports.approveComment = validate([
     param('id').not().isEmpty().toInt()
-]
+])
 
-exports.deleteComment = [
+exports.deleteComment = validate([
     param('id').not().isEmpty().toInt()
-]
+])
