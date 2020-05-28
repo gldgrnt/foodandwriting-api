1e8f4f5d52d9e235418f1cb0d79a0a0f80ead67f
Gilad Grant <ggrant966@gmail.com>
2020-05-28 11:10:05 +0100

    Add `addComment` method

--- app/models/comments.js
+++ app/models/comments.js
@@ -10,7 +10,7 @@ class CommentsModel {
      * Select all comments
      */
     async selectAll() {
-        const queryString = `SELECT * FROM ${this.table} ORDER BY date DESC`
+        const queryString = `SELECT * FROM ${this.table} ORDER BY date DESC;`
         return this.db.query(queryString)
     }
 
@@ -18,8 +18,21 @@ class CommentsModel {
      * Select by post id
      */
     async selectByPostId(postId) {
-        const queryString = `SELECT * FROM ${this.table} WHERE post_id = $1 ORDER BY date DESC`
+        const queryString = `SELECT * FROM ${this.table} WHERE post_id = $1 ORDER BY date DESC;`
         const params = [postId]
+        return this.db.query(queryString, params)
+    }
+
+    /**
+     * Add comment
+     */
+    async addComment({ displayName, postId, postSlug, parentCommentId = 0, text }) {
+        const queryString = `
+            INSERT INTO ${this.table} (display_name, post_id, post_slug, parent_comment_id, text) 
+            VALUES ($1, $2, $3, $4, $5)
+            RETURNING id;
+        `
+        const params = [displayName, postId, postSlug, parentCommentId, text]
         return this.db.query(queryString, params)
     }
 }
